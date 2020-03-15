933c4fdabf717f403aabd9cdbfa8e91d2f46d435
manju <manju@DESKTOP-C4PETOQ>
2020-03-15 18:40:34 +0530

    project1

---
+++ src/test/java/tre1/C1.java
@@ -0,0 +1,12 @@
+package tre1;
+
+import org.testng.annotations.Test;
+
+public class C1 {
+	@Test
+	public void m1() {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("HELLO WORLD");
+		}
+	}
+}
